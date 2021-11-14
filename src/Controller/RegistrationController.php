121375695fe3d976f01a0132da3015363cb52741
Pat <p171982@gmail.com>
2021-11-14 19:50:48 +0000

    some new

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -9,28 +9,56 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-
-
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegistrationController extends AbstractController
 {
     /**
      * @Route("/registration", name="registration")
      */
-    public function index(Request $request): Response
+    public function index(Request $request , UserPasswordHasherInterface $passwordHasher): Response
     {
         //create a User object and initialize some data
         $user = new User();
+        //connet to DB
+        $entityManager = $this->getDoctrine()->getManager();
         // $user->setEmail('test1@testowy.com');
         // $user->setRoles(['ROLE_USER']);
         // $user->setPassword('1234');
 
-        //create a temporary form in controller
+        //use a form created in UserType.php
         $form = $this->createForm(UserType::class, $user);
-           
+        //handle submitted form
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            //get user input data
+            $user = $form->getData();
+            //set user rola as 'ROLE_USER'
+            $user->setRoles(['ROLE_USER']);
+            //get password and HASH it
+            $plainUserPassword = $user->getPassword();
+            var_dump($plainUserPassword);
+            // hash the password (based on the security.yaml config for the $user class)
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $plainUserPassword
+            );
+            $user->setPassword($hashedPassword);
+            //tell Doctrine,you want to eventually save the User
+            $entityManager->persist($user);
+            //save user to DB Table User (INSERT INTO)
+            $entityManager->flush();
+            //redirect after submitting data
+            return new Response('You have been registered as ' . $user->getUserIdentifier() . 
+                                ' <br> email as: ' . $user->getEmail() . ' 
+                                 You can login now.' . '<a href="userlogin">Login</a>' );  
+        }
+        //return and render form
         return $this->renderForm('registration/index.html.twig', [
             'form' => $form,
+            'user' => $user,
+            // 'userRole' => $user_role,
         ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,6 +7,7 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
@@ -21,6 +22,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private $id;
 
     /**
+     * @Assert\NotBlank
      * @ORM\Column(type="string", length=180, unique=true)
      */
     private $email;
@@ -31,12 +33,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private $roles = [];
 
     /**
+     * @Assert\NotBlank
      * @var string The hashed password
      * @ORM\Column(type="string")
      */
     private $password;
 
     /**
+     * @Assert\NotBlank
      * @ORM\Column(type="string", length=255)
      */
     private $username;
