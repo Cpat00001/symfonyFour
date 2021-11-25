5a2889e22d0e2a59972ed8736231e8e7fe43485b
Pat <p171982@gmail.com>
2021-11-25 18:55:16 +0000

    changes

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,13 +11,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/product")
- */
+* @Route("/product" , name="product")
+*/
+
 class ProductController extends AbstractController
 {
-    /**
+   /**
      * @Route("/", name="product_index", methods={"GET"})
-     */
+  */
+
     public function index(ProductRepository $productRepository): Response
     {
         return $this->render('product/index.html.twig', [
