134eaccc8d9a568eba61932c9fe5f3a163cb0b20
Pat <p171982@gmail.com>
2021-11-27 20:14:54 +0000

    comments

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
-* @Route("/product" , name="product")
+* @Route("/product")
 */
 
 class ProductController extends AbstractController
